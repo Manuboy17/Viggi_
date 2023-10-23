4ffbf3d041e9b488c87ca1c03c9e26d5681c2ebe
Manuboy17 <107696896+Manuboy17@users.noreply.github.com>
2023-10-22 23:03:41 -0300

    1.4

--- Viggi/src/app/registro/registro.page.ts
+++ Viggi/src/app/registro/registro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
--- Viggi/src/environments/environment.ts
+++ Viggi/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'TU_API_KEY',
+    authDomain: 'TU_AUTH_DOMAIN',
+    projectId: 'TU_PROJECT_ID',
+    storageBucket: 'TU_STORAGE_BUCKET',
+    messagingSenderId: 'TU_MESSAGING_SENDER_ID',
+    appId: 'TU_APP_ID'
+  }
 };
 
 /*
